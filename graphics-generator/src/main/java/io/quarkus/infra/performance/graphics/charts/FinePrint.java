d8fa4edef30733964b8bdf68744e154bfacabdc6
Eric Deandrea <eric@ericdeandrea.dev>
2026-02-11 11:20:58 -0500

    Update graphics-generator/src/main/java/io/quarkus/infra/performance/graphics/charts/FinePrint.java
    Co-authored-by: Holly Cummins <holly.cummins@ibm.com>

--- graphics-generator/src/main/java/io/quarkus/infra/performance/graphics/charts/FinePrint.java
+++ graphics-generator/src/main/java/io/quarkus/infra/performance/graphics/charts/FinePrint.java
@@ -18,7 +18,7 @@ import io.quarkus.infra.performance.graphics.model.Repo;
 import io.quarkus.infra.performance.graphics.model.Timing;
 
 public class FinePrint implements ElasticElement {
-    static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss z");
+    static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
     private static final String SOURCE_CODE_LABEL = "Source:";
 
     private static final int MAXIMUM_FONT_SIZE = 30;
